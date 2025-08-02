425c911056a01d351b8e5242e3adb4f051e4b08a
Brad Majewski <bradmajewski@fastmail.com>
2025-08-02 14:27:22 -0400

    Adding Bootstrap, Bootswatch theme Brite, and simple_form.

---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,6 @@
+# From: https://github.com/rails/dartsass-rails/?tab=readme-ov-file#configuring-sourcemaps
+Rails.application.config.dartsass.build_options = [
+  '--no-charset',
+  '--embed-sources',
+  '--silence-deprecation=color-functions',
+]
